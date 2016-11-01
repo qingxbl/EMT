6e46ea6981419b0f324288ce305a388096da9de5
guopengfei <guopengfei@yy.com>
2016-11-01 10:56:30 +0800

    + EMTPoolSupport

--- src/EMTTest/main.cpp
+++ src/EMTTest/main.cpp
@@ -32,7 +32,6 @@ int run(void)
 	return 0;
 }
 
-
 int main(int /*argc*/, char* /*argv*/[])
 {
 	SetEnvironmentVariableW(L"pipeName", L"8f752ba4-0a88-48d7-bdfc-08b1dc2162a5");
@@ -45,7 +44,3 @@ int main(int /*argc*/, char* /*argv*/[])
 
 	return run();
 }
-
-EXTERN_C void * rt_memset(void *mem, const int val, const uint32_t size) { return memset(mem, val, size); }
-EXTERN_C uint16_t rt_cmpXchg16(volatile uint16_t *dest, uint16_t exchg, uint16_t comp) { return (uint16_t)InterlockedCompareExchange16((volatile SHORT *)dest, (SHORT)exchg, (SHORT)comp); }
-EXTERN_C uint32_t rt_cmpXchg32(volatile uint32_t *dest, uint32_t exchg, uint32_t comp) { return (uint32_t)InterlockedCompareExchange((volatile LONG *)dest, (LONG)exchg, (LONG)comp); }
