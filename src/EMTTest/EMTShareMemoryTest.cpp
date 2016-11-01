99d12c710579abd293535aa8b9014e6a02abd7d7
guopengfei <guopengfei@yy.com>
2016-11-01 16:24:06 +0800

    * Change EMTTest to UnitTest.

---
+++ src/EMTTest/EMTShareMemoryTest.cpp
@@ -0,0 +1,17 @@
+#include "stable.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace EMTTest
+{
+	TEST_CLASS(EMTShareMemoryTest)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+	};
+}
