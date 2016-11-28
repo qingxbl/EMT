820cd3f9ce9e4a988c55b2591b474d764f84297d
guopengfei <guopengfei@yy.com>
2016-11-28 14:40:16 +0800

    * Fix Demo.

--- src/EMTDemo/main.cpp
+++ src/EMTDemo/main.cpp
@@ -8,8 +8,9 @@
 
 enum
 {
-	kTestCount = 1000000,
-	kTestBufferSize = 128 * 1096,
+	kTestCount = 1000,
+	kTestBufferSize = 512 * 1024,
+	kTestSendConcurrent = 100,
 };
 
 static void timeUsage(const char *fmt, const FILETIME &start, const FILETIME &end)
@@ -27,16 +28,26 @@ class IPCHandler : public IEMTIPCHandler
 	IMPL_IEMTUNKNOWN;
 
 public:
+	enum HandlerType : uint32_t
+	{
+		kTypeUnknown = 0,
+		kTypeServer,
+		kTypeClient,
+	};
+
+public:
 	IPCHandler(const wchar_t * name);
 	~IPCHandler();
 
-	bool isReady() const { return mInited != 0; }
+	HandlerType handlerType() const { return mType; }
 	const wchar_t * name() const { return mName; }
 
 	IEMTThread * thread() const { return mThread.lock().get(); }
 	IEMTIPC * IPC() const { return mIPC.lock().get(); }
 
 	HANDLE handleThread() const { return mHandleThread; }
+
+	void send(const uint32_t count = 1);
 
 protected: // IEMTIPCHandler
 	virtual void connected();
@@ -49,7 +60,7 @@ private:
 	static void thread_entry(void * arg);
 
 private:
-	volatile uint32_t mInited;
+	volatile HandlerType mType;
 	const wchar_t * mName;
 	std::weak_ptr<IEMTThread> mThread;
 	std::weak_ptr<IEMTIPC> mIPC;
@@ -58,15 +69,21 @@ private:
 
 	int mReceivedCount;
 	int mSendCount;
+	int mSentCount;
 	FILETIME mTimeReceiveStart;
 	FILETIME mTimeReceiveEnd;
+	FILETIME mTimeSendStart;
+	FILETIME mTimeSendEnd;
+
+	uint8_t mInput[kTestBufferSize];
 };
 
 IPCHandler::IPCHandler(const wchar_t * name)
-	: mInited(0)
+	: mType(kTypeUnknown)
 	, mName(name)
 	, mReceivedCount(0)
 	, mSendCount(0)
+	, mSentCount(0)
 {
 	mHandleThread = (HANDLE)_beginthread(thread_entry, 0, this);
 }
@@ -80,6 +97,28 @@ IPCHandler::~IPCHandler()
 	}
 
 	while (!mThread.expired());
+}
+
+void IPCHandler::send(const uint32_t count/* = 1*/)
+{
+	if (mSendCount == 0)
+		::GetSystemTimePreciseAsFileTime(&mTimeSendStart);
+
+	IEMTIPC * ipc = IPC();
+
+	for (uint32_t i = 0; i < count; ++i)
+	{
+		if (mSendCount >= kTestCount)
+			return;
+
+		++mSendCount;
+
+		char * buf = (char *)ipc->alloc(kTestBufferSize);
+
+		memcpy(buf, mInput, sizeof(mInput));
+
+		ipc->send(buf);
+	}
 }
 
 void IPCHandler::connected()
@@ -118,10 +157,16 @@ void IPCHandler::sent(void * buf)
 {
 	//printf("[EMTIPC] %p sent.\n", buf);
 
-	++mSendCount;
-
-	if (mSendCount == kTestCount && mReceivedCount == kTestCount)
+	switch (++mSentCount)
+	{
+	case kTestCount:
+		::GetSystemTimePreciseAsFileTime(&mTimeSendEnd);
+		timeUsage("Send %llu\n", mTimeSendStart, mTimeSendEnd);
 		IPC()->disconnect();
+		break;
+	default:
+		send();
+	}
 }
 
 void IPCHandler::thread_entry(void * arg)
@@ -134,10 +179,14 @@ void IPCHandler::thread_entry(void * arg)
 	pThis->mThread = thread;
 	pThis->mIPC = ipc;
 
+	HandlerType type = kTypeClient;
 	if (!ipc->connect(pThis->mName))
+	{
+		type = kTypeServer;
 		ipc->listen(pThis->mName);
+	}
 
-	::InterlockedIncrement(&pThis->mInited);
+	::InterlockedExchange((uint32_t *)&pThis->mType, type);
 
 	thread->exec();
 }
@@ -147,29 +196,13 @@ int main(int /*argc*/, char* /*argv*/[])
 	wchar_t ipcName[] = L"EMTDemo";
 
 	IPCHandler ipcHandler(ipcName);
-	while (!ipcHandler.isReady());
+	while (ipcHandler.handlerType() == IPCHandler::kTypeUnknown);
 
 	IEMTIPC * ipc = ipcHandler.IPC();
 	while (!ipc->isConnected());
 
-	FILETIME timeSendStart;
-	FILETIME timeSendEnd;
-	char input[kTestBufferSize];
-	input[0] = 'a';
-	input[1] = '\0';
-
-	//while (scanf_s("%s", input, (unsigned)_countof(input)) != EOF)
-	::GetSystemTimePreciseAsFileTime(&timeSendStart);
-	for (int i = 0; i < kTestCount; ++i)
-	{
-		char * buf = (char *)ipc->alloc(sizeof(input));
-
-		memcpy(buf, input, sizeof(input));
-
-		ipc->send(buf);
-	}
-	::GetSystemTimePreciseAsFileTime(&timeSendEnd);
-	timeUsage("Send %llu\n", timeSendStart, timeSendEnd);
+	if (ipcHandler.handlerType() == IPCHandler::kTypeServer)
+		ipcHandler.send(kTestSendConcurrent);
 
 	::WaitForSingleObject(ipcHandler.handleThread(), INFINITE);
 
